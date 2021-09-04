514846084237829edbd1e3ce234f324e65cc8a2b
dsszymanski <dan1993us@yahoo.com>
2021-09-04 05:48:29 -0400

    Added minor styling. Added profile component with styling.

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile() {
+	return (
+		<div className='profile-container'>
+			<img className='profile-image' src='images/rhydon.jpg' alt='Profile' />
+			<h3>Daniel S Szymanski</h3>
+			<div className="profile-link-container">
+				<a href='https://github.com/DSSzymanski' className='profile-link'>Github</a>
+				<a href='https://www.linkedin.com/in/dsszymanski/' className='profile-link'>LinkedIn</a>
+			</div>
+		</div>
+	);
+}
+
+export default Profile
