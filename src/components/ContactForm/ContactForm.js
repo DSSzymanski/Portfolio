246f55c82e0dd7acc14f4d3713b3b6de0eb1e3ab
dsszymanski <dan1993us@yahoo.com>
2021-09-07 13:55:40 -0400

    Added contact information form component and design.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React from 'react';
 import '../../App.css';
 import './ContactForm.css';
 
 function ContactForm() {
     return (
-        <h1>ContactForm</h1>
+        <div className='contact-form-container'>
+            <form action='mailto:danielsszymanski@gmail.com' method='POST' encType='multipart/form-data' name='EmailForm'>
+                <div className="contact-info-container flex-center-center">
+                    <div className="contact-info">
+                            <label htmlFor='name'>Name:</label>
+                            <input type='text' id='name' size='19' name='name'/>
+                    </div>
+                    <div className="contact-info">
+                        <label htmlFor='email'>Email:</label>
+                        <input type='email' id='email' name='email'/>  
+                    </div>
+                </div>
+                <div className="contact-message">
+                    <label htmlFor='message'>Message:</label>
+                    <textarea name='message' id='message'></textarea>
+                </div>
+                <button type='submit' value='Submit'>Send</button>
+            </form>  
+        </div>
     );
 }
 
