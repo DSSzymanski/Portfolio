6479a0ae1584bd342eb1c2af1ad9b13f0a6f4d39
dsszymanski <dan1993us@yahoo.com>
2021-09-08 12:04:23 -0400

    Changed sizing in desktop view. Added some styling to the email me box.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,7 +5,8 @@ import './ContactForm.css';
 function ContactForm() {
     return (
         <div className='contact-form-container'>
-            <form action='mailto:danielsszymanski@gmail.com' method='POST' encType='multipart/form-data' name='EmailForm'>
+            <h1 className='contact-banner'>Email Me</h1>
+            <form action='mailto:danielsszymanski@gmail.com' method='POST' encType='multipart/form-data' name='EmailForm' className="contact-form">
                 <div className="contact-info-container flex-center-center">
                     <div className="contact-info">
                             <label htmlFor='name'>Name:</label>
