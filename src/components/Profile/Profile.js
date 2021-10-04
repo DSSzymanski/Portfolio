c823f30d8e3a532da513d9aadf0c7c766d6b9d9c
dsszymanski <dan1993us@yahoo.com>
2021-10-04 16:40:52 -0400

    Changed profile picture

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import '../../App.css';
 function Profile() {
 	return (
 		<div className='profile-container'>
-			<img className='profile-image' src='images/rhydon.jpg' alt='Profile' />
+			<img className='profile-image' src='images/Profile_Picture.jpg' alt='Profile' />
 			<h3>Daniel S Szymanski</h3>
 			<div className="profile-link-container">
 				<a href='https://github.com/DSSzymanski' className='profile-link'>Github</a>
