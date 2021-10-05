587595d1deae0de2bc6aac82856bc5a757262c37
dsszymanski <dan1993us@yahoo.com>
2021-10-05 12:18:01 -0400

    Updated profile page

--- src/components/ProfileTextBoxes/ProfileTextBoxItems.js
+++ src/components/ProfileTextBoxes/ProfileTextBoxItems.js
@@ -2,9 +2,9 @@ export const ProfileTextBoxItems = [
 	{
 		title: 'About Me',
 		details: "\tI'm a 28 year old programmer looking for his first job. Most of my experience lies working with a " +
-"mix of Python and Web development. In my free time I enjoy reading various articles on AI/machine learning. My goal is " +
-"to continue to develop and expand my repertoire of skills.\n\n\tOutside of programming, I enjoy fishing, hiking, " +
-"listening to music, playing chess, and reading.",
+"mix of Python and Web development. In my free time I enjoy reading various articles on AI/machine learning.\n\n\t My goal is " +
+"to obtain my first job to start my career continue to develop and expand my repertoire of skills.\n\n\tOutside of programming, "+
+"I enjoy fishing, hiking, listening to music, playing chess, and reading.",
 	},
 	{
 		title: 'Experience',
